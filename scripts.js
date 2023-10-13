2e1317e0dd00e5b2cea0467bae7666d305550c5d
Percy Meneses <46634069+percymeneses08@users.noreply.github.com>
2023-10-13 17:59:46 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-alert("Hola!")
+var elementoBoton = document.getElementById("boton")
+var elementoTitulo = document.getElementById("titulo")
+
+elementoBoton.addEventListener("click", CambiarTexto)
+
+function CambiarTexto()
+{
+  if(elementoTitulo.classList.contains("bienvenida"))
+  {
+    elementoTitulo.innerText = "¡Bienvenido a mi web!"
+    elementoTitulo.classList.remove("bienvenida")
+    elementoTitulo.classList.add("despedida")
+  }
+  else if(elementoTitulo.classList.contains("despedida"))
+  {
+    elementoTitulo.innerText = "¡Muchas gracias, hasta luego!"
+    elementoTitulo.classList.remove("despedida")
+    elementoTitulo.classList.add("bienvenida")
+  }
+}
